bd59c240283258b905825e16e19ee6f273f4c6f2
Clarity-89 <homes89@ukr.net>
2015-09-12 00:37:22 +0300

    Add start screen

--- js/engine.js
+++ js/engine.js
@@ -184,7 +184,11 @@ var Engine = (function (global) {
         'images/grass-block.png',
         'images/enemy-bug.png',
         'images/char-boy.png',
-        'images/heart_icon.png'
+        'images/heart_icon.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png'
     ]);
 
     startButton.onclick = function () {
